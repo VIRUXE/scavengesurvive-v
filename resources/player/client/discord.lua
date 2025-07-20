c1484ea04237379601f8cba9d76c99cc7bdf9c38
Flávio Pereira <flavioaspereira@gmail.com>
2025-07-20 16:14:36 +0100

    feat(player/client/discord): add Discord rich presence integration

---
+++ resources/player/client/discord.lua
@@ -0,0 +1,3 @@
+SetDiscordAppId(GetConvar('discord_app_id', ''))
+-- SetDiscordRichPresenceAsset('logo')
+SetDiscordRichPresenceAction(0, 'Discord', GetConvar('discord_invite_url', 'https://discord.gg/wTtgsTdDRy'))
