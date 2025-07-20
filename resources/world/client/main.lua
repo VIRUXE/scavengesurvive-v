5604013e09031a4dcc5c013114c71b9a4bc941f9
Flávio Pereira <flavioaspereira@gmail.com>
2025-07-20 16:11:31 +0100

    fix(world/client/main): replace SetClockTime with NetworkOverrideClockTime for time management

--- resources/world/client/main.lua
+++ resources/world/client/main.lua
@@ -3,5 +3,5 @@ SetArtificialLightsState(true)
 SetArtificialLightsStateAffectsVehicles(false)
 
 -- Clock
-SetClockTime(0, 0, 0)
+NetworkOverrideClockTime(0, 0, 0)
 PauseClock(true) -- We'll have it paused for now
