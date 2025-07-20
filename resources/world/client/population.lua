99cb7128c0a6045d0b55b63950c177265ecab70e
Flávio Pereira <flavioaspereira@gmail.com>
2025-07-20 16:10:48 +0100

    feat(world/client/population): add population control settings and density multipliers

---
+++ resources/world/client/population.lua
@@ -0,0 +1,18 @@
+SetPedPopulationBudget(0)
+SetVehiclePopulationBudget(0)
+SetGarbageTrucks(false)
+SetRandomBoats(false)
+SetCreateRandomCops(false)
+SetCreateRandomCopsNotOnScenarios(false)
+SetCreateRandomCopsOnScenarios(false)
+
+CreateThread(function()
+    while true do
+        SetScenarioPedDensityMultiplierThisFrame(0.0, 0.0)
+        SetParkedVehicleDensityMultiplierThisFrame(0.0)
+        SetVehicleDensityMultiplierThisFrame(0.0)
+        SetRandomVehicleDensityMultiplierThisFrame(0.0)
+
+        Wait(0)
+    end
+end)
