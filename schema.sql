95fdd5bb9c5c49eb9228e86e8429be663f84d6ab
Flávio Pereira <flavioaspereira@gmail.com>
2025-07-25 23:02:37 +0100

    feat(database/schema): add timestamps for created_at and updated_at in accounts and identifiers tables

--- schema.sql
+++ schema.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS `accounts` (
   `email` varchar(100) NOT NULL,
   `last_position` json DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -13,6 +14,8 @@ CREATE TABLE IF NOT EXISTS `account_identifiers` (
   `account_id` int(11) NOT NULL,
   `type` ENUM('steam', 'discord', 'xbl', 'live', 'license', 'license2', 'fivem', 'ip') NOT NULL,
   `value` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`account_id`, `type`),
   UNIQUE KEY `idx_type_value` (`type`, `value`),
   CONSTRAINT `fk_account_identifiers_account` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`) ON DELETE CASCADE
@@ -21,6 +24,8 @@ CREATE TABLE IF NOT EXISTS `account_identifiers` (
 CREATE TABLE IF NOT EXISTS `account_tokens` (
   `account_id` int(11) NOT NULL,
   `token` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`account_id`,`token`),
   KEY `account_id` (`account_id`),
   CONSTRAINT `fk_account_tokens_account` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`) ON DELETE CASCADE
