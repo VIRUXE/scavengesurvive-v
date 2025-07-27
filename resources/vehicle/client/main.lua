b9350d57ff5bea6e3ee89baa518864086a0b1d1c
Flávio Pereira <flavioaspereira@gmail.com>
2025-07-27 10:04:28 +0100

    refactor(vehicle/client): encapsulate helmet flag setting in a dedicated function

--- resources/vehicle/client/main.lua
+++ resources/vehicle/client/main.lua
@@ -1,1 +1,9 @@
-SetPedConfigFlag(cache.ped, 35, false) -- 35 is the helmet prop index, 1 disables auto helmet
+local function setDefaultPedFlags(ped)
+    SetPedConfigFlag(ped, 35, false) -- 35 is the helmet prop index, 1 disables auto helmet
+end
+
+lib.onCache('ped', function(ped, oldPed)
+    setDefaultPedFlags(ped)
+end)
+
+setDefaultPedFlags(cache.ped)
